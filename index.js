488ad61d3e14c7af4cbd249d70338f2816e0befe
sykes836 <bernard.sykes836@gmail.com>
2018-08-12 15:59:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+
+
+
+ var drivers = ["Milo", "Otis", "Garfield"]
+
+ function drivers() {
+  const drivers = [TypeError];
+}
+ 
+ function destructivelyAppendDriver(driver) {
+  drivers.push(driver);
+  return drivers;
+}
+ function destructivelyPrependDriver(driver) {
+  drivers.unshift(driver);
+  return drivers;
+}
+ function destructivelyRemoveLastDriver(driver) {
+  drivers.pop();
+  return drivers;
+}
+ function destructivelyRemoveFirstDriver(driver) {
+  drivers.shift();
+  return drivers;
+}
+ function appendDriver(driver) {
+  return [...drivers, driver];
+}
+ function prependDriver(driver) {
+  return [driver, ...drivers];
+}
+ function removeLastDriver() {
+  return drivers.slice(0, -1);
+}
+ function removeFirstDriver(){
+  return drivers.slice(1)
+}
